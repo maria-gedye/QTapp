e86d82337e5b7f2dd6a2594cff104326ef6d7d8a
maria-gedye <mariagedye@gmail.com>
2021-11-05 10:18:54 +1300

    test push 2 - added button and messagebox

--- MCR-app/mainwindow.cpp
+++ MCR-app/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,13 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox greet;
+    greet.setText("You clicked me!");
+    greet.exec();
 }
 
